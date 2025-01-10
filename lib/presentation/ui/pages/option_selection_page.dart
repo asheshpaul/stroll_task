66525b8e529e82a8c1967280ea80521860f73ab9
asheshpaul <asheshpaul64@gmail.com>
2025-01-10 16:08:31 +0530

    template, page added & folder structure updated

---
+++ lib/presentation/ui/pages/option_selection_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../templates/option_selection_template.dart';
+
+class OptionSelectionPage extends StatelessWidget {
+  const OptionSelectionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const OptionSelectionTemplate();
+  }
+}
---
+++ lib/presentation/ui/templates/option_selection_template.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OptionSelectionTemplate extends StatelessWidget {
+  const OptionSelectionTemplate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+}
